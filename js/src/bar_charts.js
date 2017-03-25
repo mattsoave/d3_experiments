9432c6f977f7e45def6f80913a66cc9669c2c05c
mattsoave <mattsoave@gmail.com>
2017-03-25 16:56:38 -0700

    basic bar chart

---
+++ js/src/bar_charts.js
@@ -0,0 +1,18 @@
+var data = [1, 2, 3, 4];
+        function initialize() {
+            d3.select(".chart")
+                .selectAll("div")
+                .data(data)
+                .enter().append("div")
+                .attr("class", "bar")
+                .style("width", function (d) {
+                    return d * 10 + "px";
+                })
+                .text(function (d) {
+                    return d;
+                });
+        }
+
+        document.addEventListener("DOMContentLoaded", function (event) {
+            initialize();
+        });
