ddca9f05cdedd624910afa4f71ff27b0276c7a5f
mattsoave <mattsoave@gmail.com>
2017-03-25 17:15:58 -0700

    random data, scaling

--- js/build/bar_charts.js
+++ js/build/bar_charts.js
@@ -1,13 +1,15 @@
 "use strict";
 
-var data = [1, 2, 3, 4];
+var data = [parseInt(Math.random() * 20), parseInt(Math.random() * 20), parseInt(Math.random() * 20), parseInt(Math.random() * 20), parseInt(Math.random() * 20)];
+
 function initialize() {
     d3.select(".chart").selectAll("div").data(data).enter().append("div").attr("class", "bar").style("width", function (d) {
-        return d * 10 + "px";
+        return x(d) + "px";
     }).text(function (d) {
         return d;
     });
 }
+var x = d3.scaleLinear().domain([0, d3.max(data)]).range([0, 500]);
 
 document.addEventListener("DOMContentLoaded", function (event) {
     initialize();
--- js/src/bar_charts.js
+++ js/src/bar_charts.js
@@ -1,18 +1,28 @@
-var data = [1, 2, 3, 4];
-        function initialize() {
-            d3.select(".chart")
-                .selectAll("div")
-                .data(data)
-                .enter().append("div")
-                .attr("class", "bar")
-                .style("width", function (d) {
-                    return d * 10 + "px";
-                })
-                .text(function (d) {
-                    return d;
-                });
-        }
+var data = [
+    parseInt(Math.random()*20),
+    parseInt(Math.random()*20),
+    parseInt(Math.random()*20),
+    parseInt(Math.random()*20),
+    parseInt(Math.random()*20)
+];
 
-        document.addEventListener("DOMContentLoaded", function (event) {
-            initialize();
+function initialize() {
+    d3.select(".chart")
+        .selectAll("div")
+        .data(data)
+        .enter().append("div")
+        .attr("class", "bar")
+        .style("width", function (d) {
+            return x(d) + "px";
+        })
+        .text(function (d) {
+            return d;
         });
+}
+var x = d3.scaleLinear()
+                .domain([0, d3.max(data)])
+                .range([0, 500]);
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    initialize();
+});
