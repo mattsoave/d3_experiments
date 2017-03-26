783196b16dc04539521f0cc5847a66756060f493
mattsoave <mattsoave@gmail.com>
2017-03-25 17:30:12 -0700

    changed data to objects

--- js/build/bar_charts.js
+++ js/build/bar_charts.js
@@ -1,15 +1,35 @@
 "use strict";
 
-var data = [parseInt(Math.random() * 20), parseInt(Math.random() * 20), parseInt(Math.random() * 20), parseInt(Math.random() * 20), parseInt(Math.random() * 20)];
+var data = [{
+    "name": "A",
+    "value": 10 + Math.random() * 20
+}, {
+    "name": "B",
+    "value": 10 + Math.random() * 20
+}, {
+    "name": "C",
+    "value": 10 + Math.random() * 20
+}, {
+    "name": "D",
+    "value": 10 + Math.random() * 20
+}, {
+    "name": "E",
+    "value": 10 + Math.random() * 20
+}, {
+    "name": "F",
+    "value": 10 + Math.random() * 20
+}];
 
 function initialize() {
     d3.select(".chart").selectAll("div").data(data).enter().append("div").attr("class", "bar").style("width", function (d) {
-        return x(d) + "px";
+        return x(d.value) + "%";
     }).text(function (d) {
-        return d;
+        return d.name + ": " + d.value.toFixed(1);
     });
 }
-var x = d3.scaleLinear().domain([0, d3.max(data)]).range([0, 500]);
+var x = d3.scaleLinear().domain([0, d3.max(data, function (d) {
+    return d.value;
+})]).range([0, 100]);
 
 document.addEventListener("DOMContentLoaded", function (event) {
     initialize();
